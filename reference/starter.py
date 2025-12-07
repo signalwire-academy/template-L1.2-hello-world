df1ea56b131e6991c91dbf67c9968d85cd440648
Brian West <brian@signalwire.com>
2025-12-07 05:19:59 -0800

    Add starter template

---
+++ reference/starter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Hello World agent starter template.
+
+Complete the TODOs to create your agent.
+"""
+
+from signalwire_agents import AgentBase
+
+# TODO: Create an agent instance with name "hello-agent"
+# agent = AgentBase(name="hello-agent")
+
+# TODO: Add a prompt section using prompt_add_section()
+# The section should have:
+#   - title: "Role"
+#   - body: A friendly greeting message
+
+# TODO: Add language configuration using add_language()
+# Configure:
+#   - name: "English"
+#   - code: "en-US"
+#   - voice: "rime.spore"
+
+# Run the agent
+if __name__ == "__main__":
+    # TODO: Uncomment when agent is created
+    # agent.run()
+    pass
